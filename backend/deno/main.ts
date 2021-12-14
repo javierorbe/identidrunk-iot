b8ccf30e52cc32889596d029ff0bcfc33d3b485d
aldayrruiz <aldayrr.ruiz@opendeusto.es>
2021-12-14 19:31:56 +0100

    Add organ

--- backend/deno/main.ts
+++ backend/deno/main.ts
@@ -1,10 +1,12 @@
 import { Application } from 'https://deno.land/x/oak/mod.ts';
+import { organ } from 'https://raw.githubusercontent.com/denjucks/organ/master/mod.ts';
 import { router } from './routes.ts';
 
 async function main() {
   const app = new Application();
 
   // Routes
+  app.use(organ());
   app.use(router.routes());
   app.use(router.allowedMethods());
 
