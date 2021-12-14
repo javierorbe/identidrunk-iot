4074e5f1c4e8841ecb34a6a4f0be928861e28179
Aldayr Ruiz <aldayr.ruiz@opendeusto.es>
2021-12-14 09:27:14 +0100

    chore: Add deno to docker compose

---
+++ backend/deno/db/connection.ts
@@ -0,0 +1,54 @@
+
+// import { config } from 'https://deno.land/x/dotenv/mod.ts';
+// app.ts
+import "https://deno.land/x/dotenv/load.ts";
+import { Client } from 'https://deno.land/x/postgres/mod.ts';
+
+// const { DB_HOST, DB_DATABASE, DB_USER, DB_PASSWORD } = config();
+const DB_HOST = Deno.env.get('DB_HOST')
+const DB_DATABASE = Deno.env.get('DB_DATABASE')
+const DB_USER = Deno.env.get('DB_USER')
+const DB_PASSWORD = Deno.env.get('DB_PASSWORD')
+
+
+// You can use the connection interface to set the connection properties
+const postgresConfig = {
+  applicationName: 'my_custom_app',
+  connection: {
+    attempts: 1,
+  },
+  database: DB_DATABASE,
+  hostname: DB_HOST,
+  password: DB_PASSWORD,
+  port: 5432,
+  user: DB_USER,
+  tls: {
+    enforce: false,
+  },
+};
+
+const client = new Client(postgresConfig);
+
+function tryToConnect(n: number) {
+  if (n === 0) {
+    console.log('Could not connect to database.');
+    return;
+  }
+
+  if (client.connected) {
+    console.log('Connected to database ✔️');
+    return;
+  }
+  console.log(
+    `Trying to connect to database ${DB_HOST} ${DB_DATABASE} ${DB_USER} ${DB_PASSWORD}`
+  );
+
+  setTimeout(async () => {
+    await client.connect();
+    tryToConnect(n--);
+  }, 3000);
+}
+const tries = 10;
+tryToConnect(tries);
+
+export { client };
