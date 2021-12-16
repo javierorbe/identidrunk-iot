0d175b0bdf708ff845387de292a28ae390c7e52b
Javier Orbe <git@javierorbe.com>
2021-12-16 15:49:23 +0100

    Update raspi endpoints

--- raspi/src/config.py
+++ raspi/src/config.py
@@ -2,8 +2,8 @@ RFID_READ_EVENT = 'rfid_read'
 ALCOHOL_READ_EVENT = 'alcohol_read'
 
 ENDPOINT = 'https://'
-AUTH_RESOURCE = ENDPOINT + '/auth'
-RESULT_RESOURCE = ENDPOINT + '/result'
+AUTH_RESOURCE = ENDPOINT + '/api/auth'
+RESULT_RESOURCE = ENDPOINT + '/api/alcohol'
 
 YELLOW = {'r': 255, 'g': 238, 'b': 88}
 RED = {'r': 239, 'g': 83, 'b': 80}
